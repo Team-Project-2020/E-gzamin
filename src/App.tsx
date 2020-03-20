f35dd673a792bf6bda16df5a69e98d6e0eda4d57
jBosak <jbosak98@gmail.com>
2020-03-20 15:38:24 +0100

    ts config, init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const x:number = 5
+  return (
+    <div className="App">
+      Hello World {x}</div>
+  );
+}
+
+export default App;
