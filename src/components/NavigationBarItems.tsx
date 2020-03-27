4e6670476cdd6d53ab7095db91d5e2debf9df919
jBosak <jbosak98@gmail.com>
2020-03-27 22:43:28 +0100

    navigation bar selected item with style

--- src/components/NavigationBarItems.tsx
+++ src/components/NavigationBarItems.tsx
@@ -6,52 +6,93 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import PeopleIcon from "@material-ui/icons/People";
 import PostAddIcon from "@material-ui/icons/PostAdd";
-import React from "react";
+import React, { useState } from "react";
 import SettingsIcon from "@material-ui/icons/Settings";
-import { NavLink } from "react-router-dom";
+import { Link as NavLink } from "react-router-dom";
 
 import "./NavigationBarItems.scss";
 
 const NavigationBarItems = () => {
+  const [selectedItem, setSelectedItem] = useState(document.location.pathname);
+
   return (
     <div className="NavigationBarItems">
-      <_MenuItem to="/" label="Home">
+      <_MenuItem
+        selectedItem={selectedItem}
+        onClick={setSelectedItem}
+        to="/"
+        label="Home"
+      >
         <DashboardIcon />
       </_MenuItem>
-      <_MenuItem to="/maketest" label="Make a test">
+      <_MenuItem
+        selectedItem={selectedItem}
+        onClick={setSelectedItem}
+        to="/maketest"
+        label="Make a test"
+      >
         <PostAddIcon />
       </_MenuItem>
-      <_MenuItem to="/group" label="Groups">
+      <_MenuItem
+        selectedItem={selectedItem}
+        onClick={setSelectedItem}
+        to="/group"
+        label="Groups"
+      >
         <PeopleIcon />
       </_MenuItem>
-      <_MenuItem to="/addquest" label="Add Questions">
+      <_MenuItem
+        selectedItem={selectedItem}
+        onClick={setSelectedItem}
+        to="/addquest"
+        label="Add Questions"
+      >
         <AddIcon />
       </_MenuItem>
-      <_MenuItem to="/settings" label="Settings">
+      <_MenuItem
+        selectedItem={selectedItem}
+        onClick={setSelectedItem}
+        to="/settings"
+        label="Settings"
+      >
         <SettingsIcon />
       </_MenuItem>
-      <_MenuItem to="/tests" label="Designed tests">
+      <_MenuItem
+        selectedItem={selectedItem}
+        onClick={setSelectedItem}
+        to="/tests"
+        label="Designed tests"
+      >
         <LayersIcon />
       </_MenuItem>
     </div>
   );
 };
 
-const _MenuItem = ({
-  to,
-  label,
-  children
-}: {
+type MenuItemType = {
   to: string;
   label: string;
   children: any;
-}) => (
-  <NavLink to={to}>
-    <ListItem button>
-      <ListItemIcon>{children}</ListItemIcon>
-      <ListItemText primary={label} />
-    </ListItem>
-  </NavLink>
-);
+  selectedItem: string;
+  onClick: (to: string) => any;
+};
+
+const _MenuItem = ({
+  to,
+  label,
+  selectedItem,
+  onClick,
+  children
+}: MenuItemType) => {
+  const selected = selectedItem === to;
+  return (
+    <NavLink to={to}>
+      <ListItem selected={selected} button onClick={() => onClick(to)}>
+        <ListItemIcon>{children}</ListItemIcon>
+        <ListItemText primary={label} />
+      </ListItem>
+    </NavLink>
+  );
+};
 
 export default NavigationBarItems;
