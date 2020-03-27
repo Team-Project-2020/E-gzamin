61a13f0293b333f7e4e836adc9ad9e180ece6b4f
jBosak <jbosak98@gmail.com>
2020-03-27 20:32:36 +0100

    links added to navBar

--- src/components/MainRoute.tsx
+++ src/components/MainRoute.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import SimpleAppBar from "./SimpleAppBar";
+import SimpleDrawer from "./SimpleDrawer";
 import AddQuest from "./AddQuest";
 import Group from "./Group";
 import Home from "./Home";
@@ -8,20 +10,34 @@ import MakeTest from "./MakeTest";
 import Settings from "./Settings";
 import Tests from "./Tests";
 
-function MainRoute() {
+type DrawerProps = {
+  isDrawerOpened: boolean;
+  setIsDrawerOpened: (drawerState: boolean) => any;
+};
+
+function MainRoute(props: DrawerProps) {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/addquest" component={AddQuest} />
-        <Route exact path="/group" component={Group} />
-        <Route exact path="/maketest" component={MakeTest} />
-        <Route exact path="/settings" component={Settings} />
-        <Route exact path="/tests" component={Tests} />
-        <Route component={Home} />
-      </Switch>
+      <SimpleAppBar {...props} />
+      <SimpleDrawer {...props} />
+      <div className="main-content">
+        <div />
+        <_MainSwitch />
+      </div>
     </Router>
   );
 }
+
+const _MainSwitch = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/addquest" component={AddQuest} />
+    <Route exact path="/group" component={Group} />
+    <Route exact path="/maketest" component={MakeTest} />
+    <Route exact path="/settings" component={Settings} />
+    <Route exact path="/tests" component={Tests} />
+    <Route component={Home} />
+  </Switch>
+);
 
 export default MainRoute;
