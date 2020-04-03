bdc514133e687ac285c9899e7afcca781b52c371
Rikain <Rikain@users.noreplay.github.com>
2020-04-03 18:07:16 +0200

    GraphiQL works now under /graphql

--- E-gzamin/Startup.cs
+++ E-gzamin/Startup.cs
@@ -16,16 +16,20 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace E_gzamin {
-    public class Startup {
-        public Startup(IConfiguration configuration) {
+namespace E_gzamin
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        public void ConfigureServices(IServiceCollection services)
+        {
             //services.AddMvc();
             services.Configure<IISServerOptions>(options =>
             {
@@ -49,10 +53,14 @@ namespace E_gzamin {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
-            } else {
+            }
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
