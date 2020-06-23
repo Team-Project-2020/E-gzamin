ce4c7f3753606cb35383a73df53d29cd4f6b6d27
jBosak <jbosak98@gmail.com>
2020-06-23 14:55:56 +0200

    firstname and lastname added to register

--- E-gzamin-frontend/src/actions/registerAction.ts
+++ E-gzamin-frontend/src/actions/registerAction.ts
@@ -1,9 +1,17 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import axios from '../lib/axiosInstance';
 
-async function register(email: string, password: string) {
+async function register(
+  email: string,
+  password: string,
+  first_name: string,
+  last_name: string,
+) {
   const response = await axios.post('/rest/users/', {
     username: email,
     password,
+    first_name,
+    last_name,
   });
   return response.status;
 }
